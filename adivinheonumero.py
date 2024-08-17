59dc610a42f9fde5d704c67518bc1a56099aa02f
Caio Andrade <163180958+andrade29@users.noreply.github.com>
2024-08-17 02:56:32 -0300

    adivinheonumero.py

--- adivinheonumero.py
+++ adivinheonumero.py
@@ -1,3 +1,4 @@
+#importando bibliotecas necessárias
 import random
 from time import sleep
 
@@ -5,16 +6,19 @@ from time import sleep
 print('Olá! Você terá que adivinhar o número que eu pensei! Escolha um número de 0 a 10.')
 numcomp = random.randint(0,10)
 
+#loop to start the game
 while True:
     numjog = int(input('Escolha um número de 0 a 10: '))
+    #loop to choose only between 0 and 10
     while numjog < 0 or numjog > 10:
         numjog = int(input('Por favor, digite apenas entre 0 e 10: '))
+    #conditional for answer is correct or not
     if numjog == numcomp:
         print(f'Parabéns, você acertou o número! Eu escolhi o número {numcomp}!')
         
     else:
         print(f'Infelizmente você errou o número! Eu escolhi o número {numcomp} e você escolheu {numjog}.')
-    
+    #asks the user if they to continue or not
     escolha = str(input('Você quer jogar de novo? [Sim/Não]: ')).strip().upper()
     while escolha not in 'SIMNÃO':
         escolha = str(input('Por favor, responda apenas com SIM ou NÃO: ')).strip().upper()
@@ -22,9 +26,3 @@ while True:
         numcomp = random.randint(0,10)
     else: 
         break
-
-    
-
-
-
-
